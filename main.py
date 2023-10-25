b6f20ba31890d6c06b13564f4dacf4dc40ba2e65
isLinXu <2267379130@qq.com>
2023-10-25 14:52:06 +0800

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 import cProfile
 import example
+import os
+import pstats
+import pandas as pd
+
+def profile_to_csv(stats_file, output_csv):
+    stats = pstats.Stats(stats_file)
+    stats_list = []
+
+    for function_info, (ccalls, ncalls, tt, ct, callers) in stats.stats.items():
+        stats_list.append({
+            "filename": function_info[0],
+            "lineno": function_info[1],
+            "function": function_info[2],
+            "ncalls": ncalls,
+            "tottime": tt,
+            "cumtime": ct,
+        })
+
+    df = pd.DataFrame(stats_list)
+    df.to_csv(output_csv, index=False)
 
 if __name__ == "__main__":
-    cProfile.run("example.main()", sort="time")
+    profile_output = "example_profile.bin"
+    cProfile.run("example.main()", profile_output)
 
+    # 将 cProfile 结果转换为 CSV 文件
+    profile_to_csv(profile_output, "example_profile.csv")
+
+    # 使用 snakeviz 可视化 cProfile 结果
+    os.system(f"snakeviz {profile_output}")
